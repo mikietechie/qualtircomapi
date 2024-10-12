71555edea72f16f5593fe11b8c8f6b64530978c0
mikietechie <mzinyoni7@outlook.com>
2024-10-13 00:47:15 +0300

    updated firebase project referense

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,15 +8,15 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
 import app from "./app";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 exports.app = onRequest(app);
